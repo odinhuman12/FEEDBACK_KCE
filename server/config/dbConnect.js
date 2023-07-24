ba39604d215005b2dac098db1cd81ce7ed805316
KRISHNA-PRAVESH <krishnapravesh.hj@gmail.com>
2023-07-24 10:10:22 +0530

    completed admin auth

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,40 +1,23 @@
-//connecting to mysql database
-const mysql = require("mysql");
 
+const mysql = require('mysql2');
 
-const connectDB = () =>{
-    try{
-        //configuration
-        const con = mysql.createConnection({
-            host:"localhost",
-            user:"root",
-            password:process.env.MYSQL_PASSWORD,
-            database:"feedback_app"
-        });
-
-        //connecting to db
-        con.connect((err)=>{
-            if(err){
-                console.log("Error in connection");
-                console.log(err);
-            }
-            else{
-                //executing query
-                const query = "SHOW TABLES";
-                con.query(query,(err,res)=>{
-                    if(err){
-                        console.log("Error in query");
-                        console.log(err);
-                    } 
-                    else console.log(res);
-                })
-            }
-        })
-       
-    }catch(err){
-        console.log(err);
-        process.exit(1);
-    }
+const dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: 'Mysql26!',
+  database: 'feedback_app',
 };
 
-module.exports = connectDB;
+const pool = mysql.createPool(dbConfig);
+
+// Test the database connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.message);
+  } else {
+    console.log('Connected to the database!');
+    connection.release();
+  }
+});
+
+module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,85 @@
 const express = require("express");
-const connectDb = require("./config/dbConnect")
+const db= require("./config/dbConnect")
+const bodyParser = require('body-parser');
 const dotenv = require("dotenv").config();
+const path = require('path');
 const app = express(); //instance of express application
 
+//telling express to serve static files from public folder
+app.use(express.static(path.join(__dirname, 'public/images')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-connectDb();
-app.get("/",(req,res)=>{
-    console.log(req);
-    res.send("Home page");
-})
+
+app.get("/login",(req,res)=>{
+    res.render('stlogin',{message:''});
+});
+
+app.post("/auth-student",(req,res)=>{
+    const {username,password} = req.body;
+    db.query('SELECT * FROM auth WHERE username = ?',[username],(err,res)=>{
+        if(err) console.log(err);
+        
+        if(res.length == 0) console.log("User doesn't exists");
+        else {
+           if(password == res[0].password) res.render("incorrect pass")
+           else console.log("Correct password");
+        }
+        
+    });
+    res.send("Request received");
+});
+
+
+//admin portal
+app.get("/admin",(req,res)=>{
+    res.render('adlogin',{message:''});
+});
+
+//authenticate admin
+app.post("/auth-admin",(req,res)=>{
+    const {username,password} = req.body;
+
+    db.query('SELECT * FROM auth WHERE username = ?',[username],(err,rs)=>{
+        if(err) console.log(err);
+        
+        if(rs.length == 0) res.render('adlogin',{message:'Incorrect UserName or password'});
+        else {
+           if(password == rs[0].password) res.render('dashboard',{message:' '});
+           else res.render('adlogin',{message:'Incorrect UserName or password'});
+        }
+        
+    });
+});
+
+
+//read excel
+
+
+
+
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on port ${process.env.PORT}`);
 });
+
+
+//utilities
+
+const userExists = (username) =>{
+    console.log("inside fn");
+    db.query(`SELECT username FROM auth WHERE username = '${username}'`,(err,res)=>{
+
+        if(err) console.log(err);
+        else{
+            if(res.length == 0) return false; //user doesn't exists
+            else return true;;
+        }
+    })
+};
+
+
+
